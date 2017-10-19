d1909908a45e5ac56a1ba7fd62ed826528c6cc83
jesusjimsa <jesusjimsa@icloud.com>
2017-10-19 19:23:09 +0300

    Count vowels

---
+++ Lab 1/ex2.py
@@ -0,0 +1,13 @@
+# Write a function that calculates how many vowels are in a string.
+
+def vowels(cadena):
+	vocales = "aeiouAEIOU"
+	count = 0
+
+	for i in cadena:
+		if(i in vocales):
+			count += 1
+	
+	return count
+
+print "Hay", vowels("Hola me llamo Jesus"), "en la frase Hola me llamo Jesus"
