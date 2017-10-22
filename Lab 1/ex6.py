7101146c375924574895e388bfc5d40b1168a858
jesusjimsa <jesusjimsa@icloud.com>
2017-10-22 18:35:56 +0300

    lowercase_with_underscores

---
+++ Lab 1/ex6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# Write a function that converts a string of
+# characters written in UpperCamelCase into
+# lowercase_with_underscores.
+
+def lowecaseUnder(unString):
+	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	continuar = False
+
+	for i in range(1, len(unString)):
+		if unString[i] in upper and not continuar:
+			unString = unString[:i] + '_' + unString[i:]	# Para insertar la barra baja antes de la mayúscula
+			continuar = True
+		else:
+			continuar = False
+	
+	unString = unString.lower()
+
+	return unString
+
+print "UpperCamelCase se convierte en", lowecaseUnder("UpperCamelCase")
