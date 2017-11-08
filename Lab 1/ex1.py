94564366575e1fce0356c656ba1f83e3f5babd60
jesusjimsa <jesusjimsa@icloud.com>
2017-11-08 10:26:13 +0200

    I don't know how to do them

--- Lab 1/ex1.py
+++ Lab 1/ex1.py
@@ -1,6 +1,3 @@
 # Find the largest common divisor of multiple numbers.
 # Define a function with variable number of parameters to resolve this.
 
-def commonDivisor(*args):
-	
-			
---
+++ Lab 1/ex8.py
@@ -0,0 +1,5 @@
+# Give a string that represents a polynomial
+# (Ex: "3x ^ 3 + 5x ^ 2 - 2x - 5") and a number
+# (whole or float). Evaluate the polynomial for
+# the given value.
+
