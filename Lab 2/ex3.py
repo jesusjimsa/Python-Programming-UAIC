624862b13b7e3694645cbcd4dfbdd708705d0ab4
jesusjimsa <jesusjimsa@icloud.com>
2017-11-08 12:45:32 +0200

    I don't understand the exercise

---
+++ Lab 2/ex3.py
@@ -0,0 +1,5 @@
+# Let a tuple (x, y) represent a point in a Cartesian system.
+# Write a function that receives as a parameter a list of
+# points and returns a list of unique tuples (a, b, c)
+# representing the unique lines determined by those points
+# ((a, b, c) corresponds to the right ax + by + c  = 0).
