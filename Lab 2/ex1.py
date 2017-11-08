4595dbc2bbfe97fd4743bbc3ac6e82d7a5ae3253
jesusjimsa <jesusjimsa@icloud.com>
2017-11-08 12:07:58 +0200

    Fibonacci

---
+++ Lab 2/ex1.py
@@ -0,0 +1,19 @@
+# Write a function to return a list of
+# the first n numbers in the Fibonacci
+# string.
+
+def Fibonacci(n):
+	Fibo = list()
+
+	if n > 0:
+		Fibo.append(1)
+	
+	if n > 1:
+		Fibo.append(1)
+
+	for i in range(2, n):
+		Fibo.append(Fibo[i - 1] + Fibo[i - 2])
+	
+	return Fibo
+
+print Fibonacci(20)
