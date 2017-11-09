ab05d291472a1642e062528870da6e31ca71b211
jesusjimsa <jesusjimsa@icloud.com>
2017-11-10 00:12:46 +0200

    WTF

---
+++ Lab 2/ex7.py
@@ -0,0 +1,10 @@
+# Write a function that receives as default a default x number
+# equal to 1, a variable number of strings, and a boolean flag
+# set to True. For each character string, generate a list
+# containing the characters that have the ASCII divisible by x
+# if the flag is set to True, otherwise it should contain
+# characters that have the non-xvid ASCII code. Example: x = 2,
+# "test", "hello", "lab002", flag = False will return
+# (["e", "s"], ["e" . Note: The function must return a variable
+# number of lists that corresponds to the number of character
+# strings received as an input.
