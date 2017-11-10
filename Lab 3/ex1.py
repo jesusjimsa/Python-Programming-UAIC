bbe52193f05044a3738d00e9a2196bd2783763f2
jesusjimsa <jesusjimsa@icloud.com>
2017-11-10 11:59:45 +0200

    Set of sets

---
+++ Lab 3/ex1.py
@@ -0,0 +1,17 @@
+# Write a function that receives as parameters two
+# lists a and b and returns a set of sets containing:
+# (a intersected with b, a reunited with b, a - b, b - a)
+
+def twoLists(aList, bList):
+	everyting = set(set())
+	aSet = frozenset(aList)
+	bSet = frozenset(bList)
+
+	everyting.add(aSet.union(bSet))
+	everyting.add(aSet.intersection(bSet))
+	everyting.add(aSet.difference(bSet))
+	everyting.add(bSet.difference(aSet))
+
+	return everyting
+
+print twoLists([3, 5, 7, 9], [3, 4, 5, 6, 7, 8, 9])
