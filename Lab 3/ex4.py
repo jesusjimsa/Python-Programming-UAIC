6907bc5a044e94156a2ed6a777643d21e528912e
jesusjimsa <jesusjimsa@icloud.com>
2017-11-15 02:08:00 +0200

    Build XML

---
+++ Lab 3/ex4.py
@@ -0,0 +1,13 @@
+# The build_xml_element function receives the following parameters:
+# tag, content, and key-value elements given as name-parameters.
+# Build and return a string that represents the corresponding XML
+# element. Example: build_xml_element("a", "Hello there",
+# href="http://python.org", _class="my-link", id="someid") =>
+# "<a href=\"http://python.org\" _class=\"my-link\" id=\"someid\">Hello there</a>"
+
+def build_xml_element(tag, content, href, clas, idd):
+	result = "<" + tag + " href=\"" + href + "\" _class=\"" + clas + "\" id=\"" + idd + "\">" + content + "</" + tag + ">"
+
+	return result
+
+print build_xml_element("a", "Hello there", "http://python.org", "my-link", "someid")
