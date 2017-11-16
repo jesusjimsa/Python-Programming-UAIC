01ff4e91d996343d4b1152268555121c18b58134
jesusjimsa <jesusjimsa@icloud.com>
2017-11-16 19:00:21 +0200

    List of words

---
+++ Lab 7/ex1.py
@@ -0,0 +1,16 @@
+# Write a function that extracts the words from a given text
+# as a parameter. A word is defined as a sequence of
+# alpha-numeric characters.
+
+import re
+
+def listOfWords(text):
+	#returns list of words in text
+
+	# both do the same
+	#re.split("\W+", text)
+
+	return re.findall("(\w+)", text)
+
+
+print listOfWords("Erase una vez")
