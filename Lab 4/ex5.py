789fdad7f8f63d8c2847df64f0b1e7a8b7773a0a
jesusjimsa <jesusjimsa@icloud.com>
2017-11-22 00:21:51 +0200

    File/directory type

---
+++ Lab 4/ex5.py
@@ -0,0 +1,42 @@
+# Write a script that receives 2 parameters from the console representing
+# a path to a directory on the system and a file name. The script will
+# recursively navigate to the file structure and directories in the given
+# directory as a parameter using os.walk and write in the file given as a
+# parameter all sites path you travelled and it's type
+# (FILE, DIRECTORY, UNKNOWN), separated by |.
+# Each path will be written on one line.
+
+import os, sys
+
+directory = raw_input("Write a path: ")
+entries = raw_input("Write the name of the new file: ")
+
+try:
+	fd = open(entries, mode = "w")
+except IOError:
+	print "Error creating or openin the file", entries
+	sys.exit()
+
+for root, directories, filenames in os.walk(directory):
+	for directory in directories:
+		to_write = os.path.join(root, directory) + " | DIRECTORY\n"
+		try:
+			fd.write(to_write)
+		except IOError:
+			print "Unable to write in", entries
+			sys.exit()
+	for filename in filenames:
+		if os.path.isfile(filename):
+			to_write = os.path.join(root,filename) + " | FILE\n"
+			try:
+				fd.write(to_write)
+			except IOError:
+				print "Unable to write in", entries
+				sys.exit()
+		else:
+			to_write = os.path.join(root,filename) + " | UNKNOWN\n"
+			try:
+				fd.write(to_write)
+			except IOError:
+				print "Unable to write in", entries
+				sys.exit()
