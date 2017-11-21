6a7ef90f990d03550e3e844d78ce3f42017197a3
jesusjimsa <jesusjimsa@icloud.com>
2017-11-21 11:06:58 +0200

    Operations with exceptions

---
+++ Lab 4/ex1.py
@@ -0,0 +1,33 @@
+# Write a python program to receive two numbers from the command line (a and b) and display:
+# a) a - b
+# b) a + b
+# c) a / b
+# d) a * b
+
+#import pdb; pdb.set_trace()
+
+first = int(raw_input("Insert the first number: "))
+second = int(raw_input("Insert the second number: "))
+
+try:	# Check if the input is correct
+	int(first)
+except ValueError:
+	print "The input was not correct, it has to be an integer"
+
+try:	# Check if the input is correct
+	int(second)
+except ValueError:
+	print "The input was not correct, it has to be an integer"
+
+try:
+	sub = first - second
+	add = first + second
+	mult = first * second
+	div = first / second
+except ArithmeticError:
+	print "One of the operations could not be completed"
+else:
+	print first, "-", second, "=", sub
+	print first, "+", second, "=", add
+	print first, "*", second, "=", mult
+	print first, "/", second, "=", div
