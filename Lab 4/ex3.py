056c39b8587d40931087618e1ff5549ac8aef383
jesusjimsa <jesusjimsa@icloud.com>
2017-11-21 20:07:26 +0200

    Save os.environ data

---
+++ Lab 4/ex3.py
@@ -0,0 +1,15 @@
+# Write a function that receives a filename as a parameter and writes
+# the data from the os.environ in the file, each line containing an
+# entry in this dictionary, in the form of the key [tab] value.
+
+import os
+#import pdb; pdb.set_trace()
+
+filename = raw_input("Enter the name of a file: ")
+fd = open(filename, mode = 'w')
+
+for a in os.environ:
+	entry = a + "\t" + os.environ[a] + "\n"
+	fd.write(entry)
+
+fd.close()
