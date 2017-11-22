a2b08fd90a5a73f7293a261470669cc0adc3394c
jesusjimsa <jesusjimsa@icloud.com>
2017-11-22 10:48:50 +0200

    Module with three functions

---
+++ Lab 4/ex7.2.py
@@ -0,0 +1,9 @@
+# ex7.2.py
+# Create your own module in which to implement at least 3 functions.
+# Use these functions in a script.
+
+import mod
+
+print mod.sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+mod.hello()
+mod.ItsWednesday()
---
+++ Lab 4/mod.py
@@ -0,0 +1,17 @@
+# ex7.1.py
+# Create your own module in which to implement at least 3 functions.
+# Use these functions in a script.
+
+def sum(*nums):
+	res = 0
+
+	for a in nums:
+		res += a
+	
+	return res
+
+def hello():
+	print "Not today"
+
+def ItsWednesday():
+	print "It's wednesday my dudes"
