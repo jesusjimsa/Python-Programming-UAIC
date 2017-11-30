042877026198328b6a46a9f21b1259b33da8ff9b
jesusjimsa <jesusjimsa@icloud.com>
2017-11-30 19:06:45 +0200

    Censor

---
+++ Lab 6/ex6.py
@@ -0,0 +1,21 @@
+# Write a function that, for a text given as a parameter, censures words that
+# begin and end with vowels. Censorship means replacing characters from odd
+# positions with *.
+
+import re
+#import pdb; pdb.set_trace()
+
+def censor(text):
+	words = re.findall("\w+", text)
+	vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+
+	for a in range(0, len(words)):
+		if words[a][0] in vowels:
+			words[a] = "*" + words[a][1:len(words[a])]
+
+		if words[a][len(words[a]) - 1] in vowels:
+			words[a] = words[a][0:len(words[a]) - 1] + '*'
+
+	return words
+
+print censor("Write a function that, for a text given as a parameter, censures words that begin and end with vowels. Censorship means replacing characters from odd positions with *.")
