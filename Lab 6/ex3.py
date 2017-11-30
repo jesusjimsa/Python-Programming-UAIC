d08eb1717686b4e8bf25074ee1c92568808a7ec7
jesusjimsa <jesusjimsa@icloud.com>
2017-11-30 18:40:54 +0200

    List of expressions

---
+++ Lab 6/ex3.py
@@ -0,0 +1,19 @@
+# Write a function that receives as a parameter a string of text
+# characters and a list of regular expressions and returns a list
+# of strings that match on at least one regular expression given
+# as a parameter.
+
+import re
+
+def listOfExpressions(characters, expressionsList):
+	solutions = dict()
+
+	for a in expressionsList:
+		solutions[a] = re.findall(a, characters)
+	
+	return solutions
+
+chars = "ertyui 1 oknb 5 vcxpv 6 cdsrtyui 3 kbvd"
+expressions = ["\d", "\w+"]
+
+print listOfExpressions(chars, expressions)
