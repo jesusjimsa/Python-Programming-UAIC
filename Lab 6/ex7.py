d197885a3b5d793d523c0487fa9f4ec87bdfe321
jesusjimsa <jesusjimsa@icloud.com>
2017-12-01 16:42:37 +0200

    Valid DNI

---
+++ Lab 6/ex7.py
@@ -0,0 +1,13 @@
+# Verify using a regular expression whether a string is a valid CNP
+
+import re
+
+def validDNI(dni):
+	return re.match("^[0-9]{8}[a-zA-Z]$", dni)	# ^ for being in the begining, $ for being in the end
+
+DNI = raw_input("Write your DNI: ")
+
+if validDNI(DNI):
+	print "Valid"
+else:
+	print "Not valid"
