d1b2d6d85f8d4bee1cbf6f32b2d9a9b4ceb517c1
jesusjimsa <jesusjimsa@icloud.com>
2017-12-02 12:38:28 +0200

    File matches regex

---
+++ Lab 6/ex8.py
@@ -0,0 +1,19 @@
+# Write a function that recursively scrolls a directory and displays those
+# files whose name matches a regular expression given as a parameter or
+# contains a string that matches the same expression. Files that satisfy
+# both conditions will be prefixed with ">>"
+
+import os, sys, re
+
+def fileMatches(directory, regex):
+	for root, directories, filenames in os.walk(directory):
+		for directory in directories:
+			print directory
+		for filename in filenames:
+			if re.match(regex, filename):
+				print ">>" + filename
+			else:
+				print filename
+
+
+fileMatches("/Users/jesusjimsa/Dropbox/Documentos/Universidad", "[a-zA-Z]*\d[a-zA-Z]*")
