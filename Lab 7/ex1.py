8d28f0a227e40abdf684685dea49845368c061e9
jesusjimsa <jesusjimsa@icloud.com>
2017-12-07 19:24:32 +0200

    Minutes passed

---
+++ Lab 7/ex1.py
@@ -0,0 +1,15 @@
+# Write a program that will write the minutes run from the start, every x
+# seconds, where x is random chosen at each iteration (from the interval
+# [a, b], where a, b are arguments). The program will run infinitely.
+
+import time, sys, random
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+
+start_time = time.time()
+
+while True:
+	time.sleep(random.randint(a, b))
+	
+	print ((time.time() - start_time) / 60), "minutes"
