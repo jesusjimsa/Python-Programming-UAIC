a0a414898d7f51fac2b5f7a1708fda381b4d1940
jesusjimsa <jesusjimsa@icloud.com>
2017-12-29 20:16:29 +0200

    Server/client socket information

---
+++ Lab 8/ex1a.py
@@ -0,0 +1,20 @@
+# Implement using the socket module (TCP), a server that writes when a client
+# connects to it the following data: the connection time in human-readable
+# format, the address, and the port of the client.
+
+import time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("127.0.0.1", 2024))
+s.listen(1)
+
+while(True):
+	print "Waiting for a client..."
+	(connection, address) = s.accept()
+
+	print "Connect address:", address
+	print "Port: 2024"
+	print (time.strftime("%H:%M:%S")), (time.strftime("%d/%m/%Y"))
+
+	connection.close()
---
+++ Lab 8/ex1b.py
@@ -0,0 +1,17 @@
+# Implement a client for the server developed at 1a: a script that receives
+# from the command line a string addr and an integer port and connects
+# through TCP to the addr address at the port port.
+
+import socket
+import sys
+
+try:
+	addr = sys.argv[1]
+	port = int(sys.argv[2])
+except:
+	print "You need to write two arguments: <server_addres> <port>"
+	sys.exit(1)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((addr, port))
+s.close()
