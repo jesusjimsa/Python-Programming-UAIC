6a93865d0d97d163af3343869f5edc13eb81253f
jesusjimsa <jesusjimsa@icloud.com>
2017-12-29 19:17:00 +0200

    Lotto 6/49

---
+++ Lab 7/ex7.py
@@ -0,0 +1,12 @@
+# Simulate lotto 6/49 draw (numbers extraction).
+
+import random
+import time
+
+print "The numbers are..."
+print "\x1B[3msound of the balls moving\x1B[23m"
+
+for i in range(0, 6):
+	time.sleep(2)
+
+	print random.randint(1, 49)
